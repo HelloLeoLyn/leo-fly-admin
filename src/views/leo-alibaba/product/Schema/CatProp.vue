136b05be823837f9975916d9f7cc09a7aa62480f
Leo <541124712@qq.com>
2023-08-21 00:34:15 +0800

    add alibaba new product add

--- src/views/leo-alibaba/product/Schema/CatProp.vue
+++ src/views/leo-alibaba/product/Schema/CatProp.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
-    <h1>{{ catProp.fields.label }}</h1>
-    <h2>{{ catProp.fields.promote }}</h2>
-    <el-form :model="value" ref="catProp" label-width="250px" :inline="false" size="normal">
-      <el-form-item v-for="option,key in catProp.fields.dataSource" :key="key"
-        :label="option.label+'['+option.uiType+']'+'['+option.required+']'" :prop="option.name"
-        :rules="option.required?[{
+    <el-form :model="value" ref="catProp" label-width="180px" :inline="false" size="normal">
+      <el-row :gutter="20">
+        <el-col :span="12" :offset="0" v-for="option, key in catProp.fields.dataSource" :key="key">
+          <el-form-item class="leo-inner-form-item" :label="option.label + '[' + option.uiType + ']' + '[' + option.required + ']'"
+            :prop="option.name" :rules="option.required ? [{
               required: true,
               message: `请输入${option.label}`,
               trigger: 'blur'
-            }]:null">
-        <template slot="label">
-          {{ option.label+'['+option.uiType+']' }}
-          <el-button type="text" size="default" @click="showNotice(option)"><i
-              class="el-icon-info" /></el-button>
-        </template>
-        <el-select v-if="option.uiType=='select'" v-model="value[option.name]" value-key="name"
-          placeholder="" clearable filterable @change="e=>handleChange(e)" @input="input">
-          <el-option v-for="item,key in option.dataSource" :key="key" :label="item.text"
-            :value="item">{{ item.text }}
-          </el-option>
-        </el-select>
-        <el-input v-else v-model="value[option.name]" @input="input"></el-input>
-      </el-form-item>
+            }] : null" inline-message>
+            <template slot="label">
+              {{ option.label }}
+              <!-- {{ option.label + '[' + option.uiType + ']' }} -->
+              <el-button type="text" size="default" @click="showNotice(option)"><i class="el-icon-info" /></el-button>
+            </template>
+            <el-select v-if="option.uiType == 'select'" v-model="value[option.name]" value-key="name" placeholder=""
+              clearable filterable @change="e => handleChange(e)" @input="input">
+              <el-option v-for="item, key in option.dataSource" :key="key" :label="item.text" :value="item">{{ item.text
+              }}
+              </el-option>
+            </el-select>
+            <el-input v-else v-model="value[option.name]" @input="input"></el-input>
+          </el-form-item></el-col>
+      </el-row>
       <el-form-item label="" size="normal">
         <el-button type="primary" @click="submitForm('catProp')">立即创建</el-button>
       </el-form-item>
@@ -34,7 +34,7 @@ export default {
   props: {
     value: {
       type: Object,
-      default: () => {}
+      default: () => { }
     },
     catProp: {
       type: Object,
@@ -78,3 +78,8 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+  .leo-inner-form-item{
+    margin-bottom: 40px;
+  }
+</style>
--- src/views/leo-alibaba/setting/index.vue
+++ src/views/leo-alibaba/setting/index.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <el-button type="primary" size="default" @click="handBltClk(opt)"
-      v-for=" opt, key in apiOptions" :key="key">{{
+    <el-button type="primary" size="default" @click="handBltClk(opt)" v-for=" opt, key in apiOptions" :key="key">{{
       opt.desc }}</el-button>
-    <el-form :model="dataBody" ref="dataBody" :rules="rules" label-width="180px" :inline="false"
-      size="normal">
+    <el-form :model="dataBody" ref="dataBody" :rules="rules" label-width="180px" :inline="false" size="normal">
       <template v-for="layoutName in  schema.hierarchy.structure[this.schema.hierarchy.root]">
         <el-form-item :key="layoutName" v-if="schema.data[layoutName].id == 'supplyType'"
           :label="schema.data[layoutName].fields.label" size="normal" prop="supplyType"
           :rules="[{ required: true, message: `请输入${schema.data[layoutName].fields.label}`, trigger: 'blur' }]">
           <el-checkbox-group v-model="dataBody.supplyType">
-            <el-checkbox v-for="item, key in schema.data[layoutName].fields.dataSource"
-              :label="item.text" :key="key" :value="item">{{
+            <el-checkbox v-for="item, key in schema.data[layoutName].fields.dataSource" :label="item.text" :key="key"
+              :value="item">{{
                 item.text
               }}<span style="color: #E6A23C">{{ item.help }}</span>
             </el-checkbox>
@@ -29,26 +27,51 @@
         <el-form-item :key="layoutName" v-else-if="schema.data[layoutName].id == 'blockProps'"
           :label="schema.data[layoutName].fields.label" size="normal">
           {{ schema.hierarchy.structure['blockProps'] }}
-          <el-form :model="dataBody" ref="blockProps" :rules="rules" label-width="80px"
-            :inline="false" size="normal">
+          <el-form :model="dataBody" ref="blockProps" :rules="rules" label-width="80px" :inline="false" size="normal">
             <template v-for="layoutName2 in  schema.hierarchy.structure['blockProps']">
               <el-form-item v-if="schema.data[layoutName2].id == 'title'" :key="layoutName2"
                 :label="schema.data[layoutName2].fields.label"
                 :rules="[{ required: true, message: `请输入${schema.data[layoutName2].fields.label}`, trigger: 'blur' }]">
                 <!-- {{ schema.data[layoutName2] }} -->
-                <el-input v-model="dataBody.title"
-                  :placeholder="schema.data[layoutName2].fields.placeholder" size="normal"
-                  :maxlength="schema.data[layoutName2].fields.maxLength" show-word-limit></el-input>
+                <el-input v-model="dataBody.title" :placeholder="schema.data[layoutName2].fields.placeholder"
+                  size="normal" :maxlength="schema.data[layoutName2].fields.maxLength" show-word-limit></el-input>
               </el-form-item>
               <el-form-item v-else-if="schema.data[layoutName2].id == 'catProp'" :key="layoutName2"
                 :label="schema.data[layoutName2].fields.label">
+                {{ schema.data[layoutName2].fields.promote.top }}
                 <CatProp v-model="dataBody.catProp" :catProp="schema.data.catProp"></CatProp>
+              </el-form-item>
+              <el-form-item v-else size="normal" :label="schema.data[layoutName2].fields.label">
+                {{ schema.data[layoutName2] }}
               </el-form-item>
             </template>
           </el-form>
         </el-form-item>
         <el-form-item :key="layoutName" v-else :label="schema.data[layoutName].fields.label">
-          {{ schema.data[layoutName] }}
+          <el-button type="primary" size="default" @click="saveAsTemplate">存为模板</el-button>
+          <el-form v-if="schema.hierarchy.structure[layoutName]" :model="dataBody" :ref="layoutName" :rules="rules"
+            label-width="80px" :inline="false" size="normal">
+            <template v-for="(layoutName2, index) in schema.hierarchy.structure[layoutName]">
+              <!-- <el-form-item :label="layoutName2" :key="index" v-if="layoutName2=='saleProp'">
+                {{ schema.data[layoutName2].fields }}
+              </el-form-item> -->
+              <el-form-item :label="schema.data[layoutName2].fields.label" :key="index" v-if="schema.data[layoutName2]"
+                :rules="schema.data[layoutName2].fields.required ? [{ required: true, message: `请输入${schema.data[layoutName].fields.label}`, trigger: 'blur' }] : null">
+                <!-- {{ handleObj(schema.data[layoutName2]).fields }} -->
+                {{ schema.data[layoutName2].fields }}
+              </el-form-item>
+              <!-- <el-form-item :label="layoutName2" :key="index" v-else>
+                {{ layoutName2 }}===========================
+              </el-form-item> -->
+            </template>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">立即创建</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
+          <div v-else>
+            {{ schema.data[layoutName] }}
+          </div>
         </el-form-item>
       </template>
       <!-- <el-form-item :label="schema.data.title.fields.label" prop="title">
@@ -125,6 +148,14 @@ export default {
           return false
         }
       })
+    },
+    saveAsTemplate() {
+
+    },
+    handleObj(obj) {
+      console.log(obj);
+      let index = Object.keys(obj).findIndex(key => key == 'fields')
+      console.log(index);
     }
   }
 }
